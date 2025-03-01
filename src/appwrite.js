ec7d073ea558ff62449aa59ffe78ce02413d8f16
Prince <princerawat7700@gmail.com>
2025-03-01 11:37:11 +0530

    inital commit

---
+++ src/appwrite.js
@@ -0,0 +1,54 @@
+import { Client, Databases, ID, Query } from 'appwrite';
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+const client = new Client()
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject(PROJECT_ID);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (searchTerm, movie) => {
+  // 1. Use Appwrite SDK to check if the search term exists in the database
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal('searchTerm', searchTerm),
+    ]);
+
+    console.log(result);
+
+    // 2. If it does, update the count
+    if (result.documents.length > 0) {
+      const doc = result.documents[0];
+
+      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+        count: doc.count + 1,
+      });
+      // 3. If it doesn't, create a new document with the search term and count as 1
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm,
+        count: 1,
+        movieId: movie.id,
+        posterUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getTrendigMovies = async () => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc('count'),
+    ]);
+
+    return result.documents;
+  } catch (error) {
+    console.error(error);
+  }
+};
